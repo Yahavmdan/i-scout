c52210d5ceaa2e622d7b7eeee89fd21f3b5dbecd
Yahav <yahav.dan@base.ai>
2025-04-24 00:09:51 +0300

    doengrade to angular 18 for deploying issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
 }
